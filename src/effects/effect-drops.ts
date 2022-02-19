0048cda61e43a46adfcbc82f31deddf7935f2657
Ryan Bucshon <busheezy@gmail.com>
2022-02-18 22:18:30 -0600

    different fades for shades

--- src/effects/effect-drops.ts
+++ src/effects/effect-drops.ts
@@ -7,14 +7,16 @@ export default function drops(x: number, y: number): SquareProperties {
   const key = `${x}.${y}`;
 
   if (cache[key]) {
-    if (random(1, 2000) === 1) {
+    if (random(1, 5000) === 1) {
       cache[key] = {
         colorIndex: random(0, 8),
         alpha: random(0, 1, true),
       };
     }
 
-    if (cache[key].alpha >= 0.01) {
+    if (cache[key].colorIndex < 2 && cache[key].alpha >= 0.01) {
+      cache[key].alpha -= cache[key].alpha * 0.0001;
+    } else if (cache[key].alpha >= 0.01) {
       cache[key].alpha -= cache[key].alpha * 0.01;
     }
 
